5b0f6c5fa315e73360443316e3e68db27cd3cb76
seokmin <msm0748@gmail.com>
2024-07-29 11:24:14 +0900

    feat: editor 기본 구성 세팅

---
+++ src/routes/+page.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import {
+		ClassicEditor,
+		Essentials,
+		Bold,
+		Italic,
+		Font,
+		Paragraph,
+		Heading,
+		Table,
+		TableColumnResize,
+		TableToolbar,
+		Image,
+		ImageCaption,
+		ImageResize,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Base64UploadAdapter
+	} from 'ckeditor5';
+
+	import 'ckeditor5/ckeditor5.css';
+	import { onMount } from 'svelte';
+
+	let editor: ClassicEditor;
+	let editorEl: HTMLDivElement;
+
+	onMount(() => {
+		if (editorEl) {
+			ClassicEditor.create(editorEl, {
+				plugins: [
+					Essentials,
+					Bold,
+					Italic,
+					Font,
+					Paragraph,
+					Table,
+					Heading,
+					Table,
+					TableColumnResize,
+					TableToolbar,
+					Image,
+					ImageCaption,
+					ImageResize,
+					ImageStyle,
+					ImageToolbar,
+					ImageUpload,
+					Base64UploadAdapter
+				],
+				toolbar: {
+					items: [
+						'heading',
+						'|',
+						'undo',
+						'redo',
+						'|',
+						'bold',
+						'italic',
+						'|',
+						'fontSize',
+						'fontFamily',
+						'fontColor',
+						'fontBackgroundColor',
+						'|',
+						'insertImage',
+						'insertTable'
+					]
+				},
+				table: {
+					contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+				},
+				image: {
+					toolbar: [
+						'imageStyle:inline',
+						'imageStyle:wrapText',
+						'imageStyle:breakText',
+						'|',
+						'imageTextAlternative',
+						'toggleImageCaption'
+					]
+				}
+			})
+				.then((newEditor) => {
+					editor = newEditor;
+				})
+				.catch(/* ... */);
+		}
+	});
+</script>
+
+<div id="editor" bind:this={editorEl}></div>
+<button
+	on:click={() => {
+		console.log(editor.getData());
+	}}>전송</button
+>
