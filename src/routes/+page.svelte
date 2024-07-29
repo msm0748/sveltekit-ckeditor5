f0a93f172dbc6c93fc494bc4f9ae2b2c6e084016
seokmin <msm0748@gmail.com>
2024-07-29 14:10:26 +0900

    feat: editor heading 커스텀

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -75,6 +75,35 @@
 						'outdent'
 					]
 				},
+				heading: {
+					options: [
+						{
+							model: 'heading2',
+							view: 'h2',
+							title: '제목 1',
+							class: 'ck-heading_heading2'
+						},
+						{
+							model: 'heading3',
+							view: 'h2',
+							title: '제목 2',
+							class: 'ck-heading_heading3'
+						},
+						{
+							model: 'heading4',
+							view: 'h3',
+							title: '제목 3',
+							class: 'ck-heading_heading4'
+						},
+						{
+							model: 'heading5',
+							view: 'h4',
+							title: '제목 4',
+							class: 'ck-heading_heading5'
+						},
+						{ model: 'paragraph', title: '본문', class: 'ck-heading_paragraph' }
+					]
+				},
 				fontFamily: {
 					options: [
 						'default',
